8e9ba62b12935e58d45167ef5a395196170c099e
ytang-eng <ytang6442@gmail.com>
2026-03-23 17:10:03 +0800

    Update ArrayUtils.java

--- src/main/java/ArrayUtils.java
+++ src/main/java/ArrayUtils.java
@@ -35,4 +35,13 @@ public class ArrayUtils {
         }
         return count;
     }
+
+    // adding method
+    public static int countOf(int[] x, int target) {
+        int count = 0;
+        for (int a: x) {
+            count = a == target ? count + 1 : count;
+        }
+        return count;
+    }
 }
